f040b7c21dfa3b7e1fe0b7744a25fbe9b9e5a2d5
Juan Benito Pacheco <juanbenito.inteliatec@gmail.com>
2016-12-26 23:59:48 +0100

    add new functions

--- the_big_game.py
+++ the_big_game.py
@@ -11,6 +11,7 @@ class GameRound():
         self.awards = get_awards()
         self.stop_round = get_stop_round()
         self.results =0
+        self.final_award = np.sum(self.awards[0:16])
 
     def get_award_round(self):
         return self.awards[self.round]
@@ -22,13 +23,13 @@ class GameRound():
         if self.round<self.stop_round:
             self.results += self.get_award_round()
             self.round += 1
-            round_label.set(str(game.round)+"/16")
-            results_label.set(self.results)
-            awards_label.set(self.get_award_round())
+            self.set_variables_label()
         else:
             round_label.set(0)
             results_label.set("Lose!")
             awards_label.set(0)
+            max_result_label.set(self.get_max_result())
+            self.disabled_button()
             results_machine_label.set(np.sum(self.awards[0:self.get_machine_round()])) if self.get_machine_round()<self.stop_round else results_machine_label.set("Machine Lost")
 
     def get_machine_round(self):
@@ -38,10 +39,40 @@ class GameRound():
         return round_stop_machine
 
     def stop_game(self):
+        max_result_label.set(self.get_max_result())
+        self.disabled_button()
         if self.get_machine_round()<self.stop_round:
             results_machine_label.set(np.sum(self.awards[0:self.get_machine_round()]))
         else:
             results_machine_label.set("Machine Lose")
+
+    def disabled_button(self):
+        next_button.config(state='disabled')
+        stop_button.config(state='disabled')
+
+    def get_final_award(self):
+        return self.final_award
+
+    def restart(self):
+        next_button.config(state='normal')
+        stop_button.config(state='normal')
+        self.round = 0
+        self.awards = get_awards()
+        self.stop_round = get_stop_round()
+        self.results = 0
+        self.final_award = np.sum(self.awards)
+        self.set_variables_label()
+        results_machine_label.set(0)
+        max_result_label.set(0)
+
+
+    def get_max_result(self):
+        return np.sum(self.awards[0:self.stop_round])
+
+    def set_variables_label(self):
+        round_label.set(str(game.round) + "/16")
+        results_label.set(self.results)
+        awards_label.set(self.get_award_round())
 
 
 def get_awards():
@@ -90,21 +121,32 @@ if __name__ == "__main__":
     awards_label = StringVar()
     results_label = StringVar()
     results_machine_label = StringVar()
+    final_award_label = StringVar()
+    max_result_label = StringVar()
+
     results_label.set(game.results)
     awards_label.set(game.get_award_round())
-    round_label.set(str(game.round)+"/16")
+    round_label.set(str(game.get_round())+"/16")
+    final_award_label.set(game.get_final_award())
 
-    Button(command=game.next_round,text="Next Round").grid(row=1)
-    Button(command=game.stop_game,text="Stop Game").grid(row=1,column=1)
+    next_button = Button(command=game.next_round,text="Next Round")
+    next_button.grid(row=1)
+    stop_button = Button(command=game.stop_game,text="Stop Game")
+    stop_button.grid(row=1,column=1)
+    Button(command=game.restart,text="New Game").grid(row=1,column=2)
 
     Label(root, text = "Round").grid(row=0,column=0)
+    Label(root, text = "Total Award:").grid(row=0,column=2)
     Label(root, textvariable = round_label).grid(row=0,column=1)
+    Label(root, textvariable = final_award_label).grid(row=0,column=3)
 
-    Label(root, textvariable = awards_label).grid(row=2,column=1)
     Label(root, text = "Award this round").grid(row=2,column=0)
-    Label(root, textvariable = results_label).grid(row=3,column=1)
+    Label(root, textvariable = awards_label).grid(row=2,column=1)
     Label(root, text = "Results").grid(row=3,column=0)
+    Label(root, textvariable = results_label).grid(row=3,column=1)
     Label(root, text="Machine Win this:").grid(row=4,column=0)
     Label(root,textvariable=results_machine_label).grid(row=4,column=1)
+    Label(root, text="Max Result").grid(row=5,column=0)
+    Label(root,textvariable=max_result_label).grid(row=5,column=1)
     root.mainloop()
 
